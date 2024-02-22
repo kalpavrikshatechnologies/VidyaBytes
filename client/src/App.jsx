d4c0801ffbd562c184433e4e9a7730a04dafa72f
Vishesh Raghuvanshi <visheshkumarraghuvanshi@gmail.com>
2024-02-22 19:29:56 +0530

    Added login route (/login) and catch-all redirect to homepage (/).

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import AdminHome from './components/AdminHome';
+import LoginSignup from './components/LoginSignup';
 
 function App() {
   return (    
       <BrowserRouter>
         <Routes>
            <Route path='/' element={<AdminHome/>}/>
+           <Route path='/login' element={<LoginSignup/>}/>
+           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
        </BrowserRouter>
 
